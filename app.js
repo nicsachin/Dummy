e090b217c38b1d70f733a0332a7922c987b9ec0c
sachin <nicsachin007@gmail>
2020-04-05 13:12:42 +0530

    2020-04-05T13:12:42+05:30

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const moment = require("moment");
 const logic = async (x , y) => {
      
     const date = moment().subtract(1 , 'd').format();
+   
     fs.writeFile("test.txt", moment().format() , ()=>{
        git.add(".").commit(date , {'--date' : date}).push();  
     });
