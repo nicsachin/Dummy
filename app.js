63e9d671de4d1816c1d741c461a57e6ab3c10c89
sachin <nicsachin007@gmail>
2020-04-06 13:07:26 +0530

    2020-04-06T13:07:26+05:30

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const git = (require("simple-git"))();
 const fs = require("fs");
 const moment = require("moment");
 
-const logic = async a => {
+
+const logic = async (x , y) => {
+     
     const date = moment().format();
     fs.writeFile("test.txt", moment().format() , ()=>{
        git.add(".").commit(date , {'--date' : date}).push();  
     });
 }
-
 
 logic()
